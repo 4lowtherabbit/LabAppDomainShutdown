938fbe990fbdb994f1399cb1506cabd733ca326d
Juntao Zhu <juzhu@microsoft.com>
2019-03-07 22:42:30 +0800

    initial

--- LabAppDomainShutdown/Global.asax.cs
+++ LabAppDomainShutdown/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +18,20 @@ namespace LabAppDomainShutdown
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_End()
+        {
+            var reason = HostingEnvironment.ShutdownReason;
+
+            if (!EventLog.SourceExists(".NET Runtime"))
+            {
+                EventLog.CreateEventSource(".NET Runtime", "Application");
+            }
+
+            EventLog log = new EventLog();
+            log.Source = ".NET Runtime";
+            log.WriteEntry($"ShutdownReason: {reason}", EventLogEntryType.Error);
         }
     }
 }
